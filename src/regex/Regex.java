a1740ca71a717d23dcc237f9b51dab0e5628966f
harshadswaghmare <98798254+harshadswaghmare@users.noreply.github.com>
2022-11-25 10:01:55 +0530

    Update Regex.java

--- src/regex/Regex.java
+++ src/regex/Regex.java
@@ -1,40 +1,36 @@
 package regex;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-
-
 public class Regex {
 
-	public static void main(String args[])
-	{
-		List<String> list1= Arrays.asList("us","United States","CALIFORNIA","Calfornia");
-		
-	  while(true) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the input value");
-		String input = sc.nextLine();
-	   if(list1.contains(input))
-		{
-			 
-	    	String value = input.replace(input, "United Kingdome America");
-	    	
-		    Pattern p = Pattern.compile(value,Pattern.CASE_INSENSITIVE);
-		    System.out.println("replacd value is : "+p);
- 		   
+	public static void main(String args[]) {
+		while (true) {
+
+			HashMap<String, String> map = new HashMap<>();
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the input value");
+			String input = sc.nextLine();
+			map.put("us", "United Kingdome America");
+			map.put("United States", "United Kingdome America");
+
+			if (map.containsKey(input)) {
+				System.out.println(map);
+				String value = input.replace(input, map.get(input));
+				Pattern p = Pattern.compile(value, Pattern.CASE_INSENSITIVE);
+				System.out.println("replacd value is : " + p);
+
+			}
+
+			else {
+				System.out.println("invalid input");
+				System.out.println(
+						"please try the following options" + "\nus or United States or CALIFORNIA or California");
+			}
+
 		}
-	   
-	   else {
-		   System.out.println("invalid input");
-		   System.out.println("please try the following options"+"\nus or United States or CALIFORNIA or California");
-	   }
-		
-		
 	}
-	}
-	
 
 }
