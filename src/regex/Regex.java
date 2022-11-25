6d0b05d1d5cb2d64c9374c7d73313e6394a4fe5b
harshadswaghmare <98798254+harshadswaghmare@users.noreply.github.com>
2022-11-25 17:54:36 +0530

    Update Regex.java

--- src/regex/Regex.java
+++ src/regex/Regex.java
@@ -34,3 +34,59 @@ public class Regex {
 	}
 
 }
+
+
+//Aproach Second for replacing application
+
+
+package Regex1;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class Regex1 {
+public static void main(String []args)
+{
+	while(true)
+	{
+	Scanner sc = new Scanner(System.in);
+	System.out.println("enter  input");
+	String input = sc.nextLine();
+	String forUS="(([Cc]alifornia|us|US|CALIFORNIA))";
+	String forIndia ="(?i)IND|india";
+    String afg ="(?i)AFG";
+	Pattern pattern = Pattern.compile("",Pattern.CASE_INSENSITIVE|Pattern.DOTALL|Pattern.MULTILINE);
+	Matcher matcher = pattern.matcher(input);
+
+	if(pattern.matches(forUS, input)) 
+	{
+      String valueString = input.replaceAll(input, "United States of America");
+      System.out.println(valueString);
+    }
+	
+	else if(pattern.matches(forIndia, input))
+	{
+		String valueString = input.replaceAll(input, "India");
+	    System.out.println(valueString);
+	}
+    
+	else if(pattern.matches(afg,input))
+	{
+		String valueString = input.replaceAll(input, "Afghanistan");
+	    System.out.println(valueString);
+	}
+    else{
+    	System.out.println("please enter valid one");
+    	System.out.println("for india ener INDIA or IND or india");
+    	System.out.println("for US enter CALIFORNIA or california or us or US");
+    	System.out.println("for Afganistan Enter AFG");
+    	
+    }
+	
+	
+	}
+  }
+}
+
+
+
