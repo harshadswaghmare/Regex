13838705435932c39c1697fb501a930fdb7b6d22
harshadswaghmare <98798254+harshadswaghmare@users.noreply.github.com>
2022-11-29 10:02:25 +0530

    Update Regex.java

--- src/regex/Regex.java
+++ src/regex/Regex.java
@@ -1,41 +1,70 @@
-package regex;
+package Regex1;
 
-import java.util.HashMap;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class Regex {
+public class Regex1 {
 
-	public static void main(String args[]) {
+	public static void main(String[] args) {
+
 		while (true) {
-
-			HashMap<String, String> map = new HashMap<String,String>();
 			Scanner sc = new Scanner(System.in);
-			System.out.println("Enter the input value");
+			System.out.println("enter  input");
 			String input = sc.nextLine();
-			map.put("us", "United Kingdome America");
-			map.put("United States", "United Kingdome America");
+			String forUS = "((?i)us|california)$";
+			String forIndia = "(?i)IND|india";
+			String afg = "(?i)AFG";
+			// String country = "[^+0-9\n]+[^(?:\s+0-9)*]";
+			// String demo ="[a-zA-Z\s]|";
 
-			if (map.containsKey(input)) {
-				System.out.println(map);
-				String value = input.replace(input, map.get(input));
-				Pattern p = Pattern.compile(value, Pattern.CASE_INSENSITIVE);
-				System.out.println("replacd value is : " + p);
+			if (Pattern.matches(forUS, input.trim())) {
+				String valueString = input.replaceAll(input, "United States of America");
+				System.out.println(valueString);
+			}
 
+			else if (Pattern.matches(forIndia, input.trim())) {
+				String valueString = input.replaceAll(input, "India");
+				System.out.println(valueString);
+			}
+
+			else if (Pattern.matches(afg, input.trim())) {
+				String valueString = input.replaceAll(input, "Afghanistan");
+				System.out.println(valueString);
+				;
 			}
 
 			else {
-				System.out.println("invalid input");
-				System.out.println(
-						"please try following options" + "\nus or United States or CALIFORNIA or California");
-			}
+				System.out.println("Enter Valid input");
+				
+				while(true) {
+				int option;
+				try {
+					System.out.println("for which country you want to enter data");
+					System.out.println("1 .India" + "\n2.Us or California" + "\n3.Afganistan");
+					option = sc.nextInt();
+					switch (option) {
+					case 1:
+						System.out.println("Please enter ind or IND or INDIA for ");
+						break;
+					case 2:
+						System.out.println("Please enter us or california or US or CALIFORNIA for united state");
+						break;
+					case 3:
+						System.out.println(" Please enter AFG for Afganistan");
+						break;
+					default:
+						System.out.println("invalid match");
+						break;
+					}
+				
 
+				} catch (Exception e) {
+					System.out.println("enter valid input");
+					break;
+				}
+				}
+
+			}
 		}
 	}
-
 }
-
-
-
-
-
